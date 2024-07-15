393c5f12ef3423876f67c5e443970d406318bc85
procoder0001 <weichen.1994.0613@gmail.com>
2024-07-15 21:43:40 +0900

    create: new component support

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import useSWR from 'swr';
 
 import useLayoutService from '@/lib/hooks/useLayout';
-
+  
 const Sidebar = () => {
   const { toggleDrawer } = useLayoutService();
   const {
